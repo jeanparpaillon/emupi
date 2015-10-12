3c303e0c2498c5cb598b4837e0dcd9209146eeaf
Jean Parpaillon <jean.parpaillon@free.fr>
2015-10-12 15:03:07 +0200

    Override emupi in docker with the one repository if launched from start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+basedir=$(cd $(dirname $0) && pwd)
+
 function _usage {
     echo "Usage: $0 container /path/to/img"
 }
@@ -18,6 +20,6 @@ fi
 container=$1
 
 xhost +
-docker run --privileged -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $2:/rpi.img $1
+docker run --privileged -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $2:/rpi.img -v $basedir/emupi:/root/emupi $1
 
 exit 0
